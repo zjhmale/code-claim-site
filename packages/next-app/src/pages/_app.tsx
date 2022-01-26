71981df90b01d66359931c50732de6334765ac4d
Martin Beierling-Mutz <mbeierling@gmail.com>
2022-01-26 23:46:38 +0100

    Allow different chains based on env
    Localhost, Rinkeby & Mainnet for now.

--- packages/next-app/src/pages/_app.tsx
+++ packages/next-app/src/pages/_app.tsx
@@ -3,17 +3,33 @@ import "@fontsource/zen-kaku-gothic-new";
 
 import { theme } from "@/chakra.config";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Provider, chain, defaultChains } from "wagmi";
+import { Provider, chain, defaultChains, Chain } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 
 import type { AppProps } from "next/app";
 
-const infuraId = process.env.WALLET_CONNECT_ID;
-const chains = [chain.mainnet];
+const infuraId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID;
+const chainName = process.env.NEXT_PUBLIC_CHAIN_NAME || "localhost";
+
+const chains: Chain[] = [];
+switch (chainName.toLowerCase().trim()) {
+  case "localhost":
+    chains.push(chain.localhost);
+    break;
+  case "rinkeby":
+    chains.push(chain.rinkeby);
+    break;
+  case "mainnet":
+    chains.push(chain.mainnet);
+    break;
+  default:
+    console.error("Unsupported chainName:", chainName);
+    break;
+}
+
 // Set up connectors
 const connectors = ({ chainId }: any) => {
-  const rpcUrl = chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ?? chain.mainnet.rpcUrls[0];
   return [
     new InjectedConnector({ chains }),
     new WalletConnectConnector({
