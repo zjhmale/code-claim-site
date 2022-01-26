20ece28758b9fda650b9dcf849e0ff5a4f0421d3
Martin Beierling-Mutz <mbeierling@gmail.com>
2022-01-26 23:47:01 +0100

    Fix deployment & make ClaimCard show after connect

--- packages/next-app/src/components/MainBox.tsx
+++ packages/next-app/src/components/MainBox.tsx
@@ -21,13 +21,25 @@ export const MainBox = ({ isConnected, isUnsupported }: MainBoxProps) => {
 
   return (
     <div>
-      <Confetti width={width} height={height} />
+      <Confetti width={width} height={height} hidden />
       <Box my={["24px", "0"]} w="100%">
-        <Heading as="h1" color="white" fontSize={["44px", "96px"]} fontWeight="500">
+        <Heading
+          as="h1"
+          color="white"
+          fontSize={["44px", "96px"]}
+          fontWeight="500"
+        >
           Airdrop
         </Heading>
-        <Text mt="5" mb="8" color="white" fontSize={["20px", "24px"]} fontWeight="500">
-          <span style={{ fontStyle: "italic" }}>$CODE</span> is the new governance token for Developer DAO. Connect your wallet to determine
+        <Text
+          mt="5"
+          mb="8"
+          color="white"
+          fontSize={["20px", "24px"]}
+          fontWeight="500"
+        >
+          <span style={{ fontStyle: "italic" }}>$CODE</span> is the new
+          governance token for Developer DAO. Connect your wallet to determine
           your airdrop eligibility.
         </Text>
         <Flex direction={["column", "row"]}>
--- packages/next-app/src/pages/index.tsx
+++ packages/next-app/src/pages/index.tsx
@@ -2,31 +2,48 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useCallback, useEffect, useState } from "react";
 import { ethers } from "ethers";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Fade, SlideFade } from "@chakra-ui/react";
 
 import { useAccount, useConnect, useNetwork } from "wagmi";
 import { CODEToken__factory } from "@/typechain";
 import { hasEthereum } from "@/utils";
 
-import { ClaimCard, ClaimCardData, ClaimCardState } from "@/components/ClaimCard";
+import {
+  ClaimCard,
+  ClaimCardData,
+  ClaimCardState,
+} from "@/components/ClaimCard";
 import { Logo } from "@/components/Logo";
 import { MainBox } from "@/components/MainBox";
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+declare let window: any;
+
+// This will either get the contract address from env or use default local hardhat address for deployed contract
+const contractAddress =
+  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ||
+  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const Home: NextPage = () => {
   const [claimPeriodEnds, setClaimPeriodEnds] = useState(0);
   const [{ data, error, loading }, switchNetwork] = useNetwork();
-  const [{ data: connectData, error: connectError }, connect] = useConnect();
   const [{ data: accountData }, disconnect] = useAccount({
     fetchEns: true,
   });
-  const isConnected = accountData && Object.entries(accountData).length > 0;
+  const isConnected =
+    typeof accountData !== "undefined" &&
+    Object.entries(accountData).length > 0;
+
+  let address = "";
+  if (accountData?.address) {
+    address = `${accountData.address.slice(0, 6)}...${accountData.address.slice(
+      -4
+    )}`;
+  }
 
   const claimCardData: ClaimCardData = {
     state: ClaimCardState.unclaimed,
-    address: "jazza.eth",
-    avatar: "",
+    address: accountData?.ens?.name || address || "",
+    avatar: accountData?.ens?.avatar || "",
     allocations: {
       member: "400",
       voterOrPoap: "0",
@@ -38,7 +55,10 @@ const Home: NextPage = () => {
   async function fetchStore() {
     if (hasEthereum()) {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const tokenContract = CODEToken__factory.connect(contractAddress, provider);
+      const tokenContract = CODEToken__factory.connect(
+        contractAddress,
+        provider
+      );
       try {
         const data = await tokenContract.claimPeriodEnds();
         setClaimPeriodEnds(data.toNumber());
@@ -58,16 +78,35 @@ const Home: NextPage = () => {
         <title>$CODE Claim Page</title>
       </Head>
       <Flex direction="row" flexWrap="wrap">
-        <Box w={{ base: "100vw", lg: "50vw" }} h="100vh" m="0" pl={["24px", "5vw"]} pr={["40px", "8vw"]} background="#08010D">
+        <Box
+          w={{ base: "100vw", lg: "50vw" }}
+          h="100vh"
+          m="0"
+          pl={["24px", "5vw"]}
+          pr={["40px", "8vw"]}
+          background="#08010D"
+        >
           <Box mt={["32px", "48px"]} mb="22vh">
             <Logo />
           </Box>
-          <MainBox isConnected={isConnected} isUnsupported={data.chain?.unsupported} />
+          <MainBox
+            isConnected={isConnected}
+            isUnsupported={!!data.chain?.unsupported}
+          />
         </Box>
-        <Flex w={{ base: "100vw", lg: "50vw" }} h="100vh" m="0" backgroundColor="#F1F0F5" align="center" justifyContent="center">
-          <Box m={["24px", "10vw"]}>
-            <ClaimCard data={claimCardData} />
-          </Box>
+        <Flex
+          w={{ base: "100vw", lg: "50vw" }}
+          h="100vh"
+          m="0"
+          backgroundColor="#F1F0F5"
+          align="center"
+          justifyContent="center"
+        >
+          <SlideFade in={isConnected} offsetY="20px">
+            <Box m={["24px", "10vw"]}>
+              <ClaimCard data={claimCardData} />
+            </Box>
+          </SlideFade>
         </Flex>
       </Flex>
     </Box>
