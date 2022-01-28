ef1cdce3f029a2c9213f00e0e4287d7fd861ff3c
JD <jann.driessen@gmx.de>
2022-01-27 21:50:12 -0500

    Update logo component to be responsive and hide text for mobile

--- packages/next-app/src/components/Logo.tsx
+++ packages/next-app/src/components/Logo.tsx
@@ -1,16 +1,24 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image, Text, useBreakpointValue } from "@chakra-ui/react";
 
-export const Logo = () => (
-  <Flex align="center">
-    <Image
-      src="assets/devdao-logo.svg"
-      alt="Developer DAO logo"
-      w="48px"
-      h="48px"
-      mr="16px"
-    />
-    <Text color="white" fontSize="24px" fontWeight="500">
-      Developer DAO
-    </Text>
-  </Flex>
-);
+export const Logo = () => {
+  const sm = "web";
+  const breakpointValue = useBreakpointValue({ base: "mobile", sm });
+  const isWeb = breakpointValue === sm;
+
+  return (
+    <Flex align="center">
+      <Image
+        src="assets/devdao-logo.svg"
+        alt="Developer DAO logo"
+        w="48px"
+        h="48px"
+        mr="16px"
+      />
+      {isWeb && (
+        <Text color="white" fontSize="24px" fontWeight="500">
+          Developer DAO
+        </Text>
+      )}
+    </Flex>
+  );
+};
