019761ead0fe636de310911c4351aac840298dfd
JD <jann.driessen@gmx.de>
2022-01-27 21:57:20 -0500

    Prettier

--- packages/next-app/src/components/MainBox.tsx
+++ packages/next-app/src/components/MainBox.tsx
@@ -13,32 +13,39 @@ export const MainBox = ({ isConnected, isUnsupported }: MainBoxProps) => {
   };
 
   return (
-    <Box my={["24px", "0"]} w="100%">
-      <Heading
-        as="h1"
-        color="white"
-        fontSize={["44px", "96px"]}
-        fontWeight="500"
-      >
-        Airdrop
-      </Heading>
-      <Text
-        mt="5"
-        mb="8"
-        color="white"
-        fontSize={["20px", "24px"]}
-        fontWeight="500"
-      >
-        <span style={{ fontStyle: "italic" }}>$CODE</span> is the new governance
-        token for Developer DAO. Connect your wallet to determine your airdrop
-        eligibility.
-      </Text>
-      <Flex direction={["column", "row"]}>
-        <Box mb={["4", "0"]} mr={["0", "7"]} w={["100%", "inherit"]}>
-          <Wallet isConnected={isConnected} isUnsupported={isUnsupported} />
-        </Box>
-        <Button label="LEARN MORE" />
-      </Flex>
-    </Box>
+    <div>
+      <Confetti
+        width={windowSize.width}
+        height={windowSize.height}
+        colors={["#DD95FF"]}
+      />
+      <Box my={["24px", "0"]} w="100%">
+        <Heading
+          as="h1"
+          color="white"
+          fontSize={["44px", "96px"]}
+          fontWeight="500"
+        >
+          Airdrop
+        </Heading>
+        <Text
+          mt="5"
+          mb="8"
+          color="white"
+          fontSize={["20px", "24px"]}
+          fontWeight="500"
+        >
+          <span style={{ fontStyle: "italic" }}>$CODE</span> is the new
+          governance token for Developer DAO. Connect your wallet to determine
+          your airdrop eligibility.
+        </Text>
+        <Flex direction={["column", "row"]}>
+          <Box mb={["4", "0"]} mr={["0", "7"]} w={["100%", "inherit"]}>
+            <Wallet isConnected={isConnected} isUnsupported={isUnsupported} />
+          </Box>
+          <Button label="LEARN MORE" />
+        </Flex>
+      </Box>
+    </div>
   );
 };
