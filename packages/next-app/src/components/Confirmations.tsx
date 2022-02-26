2c32ef6946b204cf58935abae0615e35aedf9d15
Martin Beierling-Mutz <mbeierling@gmail.com>
2022-02-26 21:15:22 +0100

    Fix build error

--- packages/next-app/src/components/Confirmations.tsx
+++ packages/next-app/src/components/Confirmations.tsx
@@ -13,7 +13,7 @@ interface ConfirmationsType {
  * Example usage = <Confirmations hash="0x2417d52765ce3b893b978c8521a20607bf4c3cabcb33ae2073e9056d397dec22" />
  */
 export const Confirmations = ({ hash }: ConfirmationsType) => {
-  const confirmations = useConfirmations(hash);
+  const confirmations = useConfirmations(hash, 20);
 
   useEffect(() => {}, [confirmations]);
 
