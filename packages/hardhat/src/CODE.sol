ed07a849f9215706235ff26abc820d6dbda127e9
Marius Baisan <baisan.marius@gmail.com>
2022-03-18 17:29:34 +0200

    setup default admin to the deployer

--- packages/hardhat/src/CODE.sol
+++ packages/hardhat/src/CODE.sol
@@ -9,7 +9,9 @@ contract CODE is ERC20, ERC20Permit, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    constructor() ERC20("Developer DAO", "CODE") ERC20Permit("Developer DAO") {}
+    constructor() ERC20("Developer DAO", "CODE") ERC20Permit("Developer DAO") {
+	_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
 
     function mint(address _to, uint256 _amount) external onlyRole(MINTER_ROLE) {
         _mint(_to, _amount);
