ed091ce1a50a440d616567685daa1565fc738dd3
Martin Beierling-Mutz <mbeierling@gmail.com>
2022-03-23 23:45:05 +0100

    Use fallback font-display

--- packages/next-app/src/pages/_document.tsx
+++ packages/next-app/src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/google-font-display */
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -7,7 +8,11 @@ class MyDocument extends Document {
         <Head>
           {/* This will use Font optimization: https://nextjs.org/docs/basic-features/font-optimization */}
           <link
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&family=Zen+Kaku+Gothic+New:wght@500;900&display=optional"
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&display=fallback"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@500;900&display=fallback"
             rel="stylesheet"
           />
         </Head>
