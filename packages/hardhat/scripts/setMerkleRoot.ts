c5e3779cd2900116f86dd05d3c9ce7be7d740a9c
0xjhtdm <zjhsdtc@gmail.com>
2022-04-21 02:08:20 +0800

    fix: update setMerkleRoot script

--- packages/hardhat/scripts/setMerkleRoot.ts
+++ packages/hardhat/scripts/setMerkleRoot.ts
@@ -1,5 +1,5 @@
 import { ethers, getNamedAccounts } from 'hardhat';
-import { CODEToken } from '../../next-app/src/typechain';
+import { ClaimCODE } from '../../next-app/src/typechain';
 
 const args = process.argv.slice(2);
 const merkleRoot = args[0];
@@ -7,7 +7,7 @@ const merkleRoot = args[0];
 async function main() {
   const { deployer } = await getNamedAccounts();
 
-  const tokenContract = <CODEToken>await ethers.getContract('CODEToken', deployer);
+  const tokenContract = <ClaimCODE>await ethers.getContract('ClaimCODE', deployer);
   const tx = await tokenContract.setMerkleRoot(merkleRoot);
   console.log(`Setting merkle root "${merkleRoot}"`);
   await tx.wait();
