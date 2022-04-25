bbd3f8072cbf2bee4b18000abd2082a829f1db78
0xjhtdm <zjhsdtc@gmail.com>
2022-04-25 19:02:11 +0800

    Fixes #4 add airdrop sum check script

---
+++ packages/hardhat/scripts/airdropSumCheck.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+import { assert } from 'console';
+
+const args = process.argv.slice(2);
+const airdropSumPath = args[0];
+
+if (!airdropSumPath) throw new Error('Missing airdropSumPath as first argument!');
+
+const EXPECTED_SUM = 3_500_000;
+
+async function main() {
+  const airdropData = JSON.parse(fs.readFileSync(airdropSumPath).toString());
+
+  const airdropSum = Object.entries(airdropData.airdrop).reduce((acc, entry) => {
+    return acc + (entry[1] as number);
+  }, 0);
+  console.log(airdropSum);
+
+  assert(airdropSum == EXPECTED_SUM);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
